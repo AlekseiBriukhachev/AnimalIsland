dc1db2deb5b91e8f7e5ffb56ce14d2aa3ca8d3c9
Aleksei Briukhachev <abriukhachev@trask.cz>
2022-12-06 21:34:23 +0100

    update

---
+++ src/com/briukhachev/aleksei/Location.java
@@ -0,0 +1,10 @@
+package com.briukhachev.aleksei;
+
+public class Location {
+    private int x;
+    private int y;
+    public Location(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/com/briukhachev/aleksei/animals/Horse.java
@@ -0,0 +1,70 @@
+package com.briukhachev.aleksei.animals;
+
+import com.briukhachev.aleksei.Location;
+import com.briukhachev.aleksei.Params;
+import com.briukhachev.aleksei.animalplant.Herbivores;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Horse extends Herbivores {
+    private double weight;
+    private int speed;
+    private double foodQuantity;
+    private Params params = new Params();
+    private List<Horse> horses = new ArrayList<>();
+
+    private Horse(double weight, int speed, double foodQuantity){
+        this.weight = weight;
+        this.speed = speed;
+        this.foodQuantity = foodQuantity;
+    }
+
+    @Override
+    public List<Horse> createNew() {
+        if (horses.size() < params.getHorseCount()){
+            horses.add(new Horse(400, 20, 60));
+            return horses;
+        }
+        return null;
+    }
+
+    @Override
+    public void move() {
+        for (Horse horse : horses) {
+            int x = ThreadLocalRandom.current().nextInt(speed);
+            int y = ThreadLocalRandom.current().nextInt(speed);
+            horse.setLocation(new Location(x, y));
+        }
+
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public double getFoodQuantity() {
+        return foodQuantity;
+    }
+
+    public void setFoodQuantity(double foodQuantity) {
+        this.foodQuantity = foodQuantity;
+    }
+}
