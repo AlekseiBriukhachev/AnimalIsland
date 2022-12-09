242962b525bc28d7100d0ee5aff37a159f6da95a
Aleksei Briukhachev <abriukhachev@trask.cz>
2022-12-09 14:48:15 +0100

    update

---
+++ src/com/briukhachev/aleksei/AnimalImage.java
@@ -0,0 +1,25 @@
+package com.briukhachev.aleksei;
+
+
+import java.awt.*;
+
+public enum AnimalImage {
+    BEAR,
+    BOAR,
+    BUFFALO,
+    CATERPILLAR,
+    DEER,
+    DUCK,
+    EAGLE,
+    FOX,
+    GOAT,
+    HORSE,
+    MOUSE,
+    RABBIT,
+    SHEEP,
+    SNAKE,
+    WOLF,
+    PLANT,
+    EMPTY
+}
+
