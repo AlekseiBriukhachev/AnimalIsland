95438c18cbf2f53289faaa66e16c43afd1ac4f4a
Aleksei Briukhachev <abriukhachev@trask.cz>
2023-03-13 18:29:15 +0100

    Add controller, begin of connection of modules with view

--- src/main/java/com/aleksei/animalisland/Application.java
+++ src/main/java/com/aleksei/animalisland/Application.java
@@ -3,7 +3,6 @@ package com.aleksei.animalisland;
 
 import com.aleksei.animalisland.utils.enumartion.AnimalType;
 import com.aleksei.animalisland.utils.factories.AnimalFactory;
-import com.aleksei.animalisland.view.MainWindow;
 import lombok.extern.slf4j.Slf4j;
 
 import java.awt.*;
@@ -25,9 +24,5 @@ public class Application {
         log.debug("Stop application");
 
 
-        EventQueue.invokeLater(() -> {
-            MainWindow mainWindow = new MainWindow();
-//            mainWindow.init();
-        });
     }
 }
