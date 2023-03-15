d055c36dee1abb25d23d4497cd5389977dcc9696
Aleksei Briukhachev <abriukhachev@trask.cz>
2023-03-15 12:42:13 +0100

    Add enum

---
+++ src/main/java/com/aleksei/animalisland/utils/AnimalType.java
@@ -0,0 +1,23 @@
+package com.aleksei.animalisland.utils.enumartion;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public enum AnimalType {
+    BEAR,
+    BOAR,
+    BUFFALO,
+    CATERPILLAR,
+    DEER,
+    DUCK,
+    EAGLE,
+    FOX,
+    GOAT,
+    HORSE,
+    MOUSE,
+    RABBIT,
+    SHEEP,
+    SNAKE,
+    WOLF;
+
+}
