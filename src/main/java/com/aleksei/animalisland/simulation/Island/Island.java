32755dcb558e655a7f22f9660328c8db975e46fb
Aleksei Briukhachev <abriukhachev@trask.cz>
2023-03-22 21:38:23 +0100

    Update island creation

--- src/main/java/com/aleksei/animalisland/simulation/Island/Island.java
+++ src/main/java/com/aleksei/animalisland/simulation/Island/Island.java
@@ -4,10 +4,16 @@ package com.aleksei.animalisland.simulation.Island;
 import com.aleksei.animalisland.config.AppConfig;
 import com.aleksei.animalisland.dao.SimulationResult;
 import com.aleksei.animalisland.simulation.Simulation;
+import com.aleksei.animalisland.simulation.animals.Animal;
+import com.aleksei.animalisland.simulation.animals.AnimalAnnotation;
+import com.aleksei.animalisland.simulation.plant.Grass;
+import com.aleksei.animalisland.simulation.plant.Plant;
+import com.aleksei.animalisland.utils.AnimalFactory;
+import com.aleksei.animalisland.utils.enums.AnimalType;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 @Data
 public class Island implements Simulation {
@@ -15,19 +21,41 @@ public class Island implements Simulation {
     private final int width = CONFIG.getIslandWidth();
     private final int height = CONFIG.getIslandHeight();
     private final Location[][] island = new Location[width][height];
-    public Island(){
+
+    public Island() {
     }
-    private void init(){
+
+    private void init() {
         for (int x = 0; x < height; x++) {
             for (int y = 0; y < width; y++) {
-                island[x][y] = new Location();
+                Location location = new Location();
+                island[x][y] = location;
+                addEntity(location);
+
             }
         }
-        addAnimal();
     }
 
-    private void addAnimal() {
+    private void addEntity(Location location) {
 
+        int animalType = ThreadLocalRandom.current().nextInt(AnimalType.values().length + 1);
+        int maxNumberOnCell = 0;
+        int number;
+
+
+        if (animalType <= AnimalType.values().length) {
+
+            Animal animal = AnimalFactory.getInstance().create(AnimalType.values()[animalType]);
+
+            AnimalAnnotation animalAnnotation = animal.getClass().getAnnotation(AnimalAnnotation.class);
+            maxNumberOnCell = animalAnnotation.maxNumberPerLocation();
+
+            number = (int) (Math.random() * maxNumberOnCell);
+            IntStream.rangeClosed(0, number).forEachOrdered(value -> location.getAnimals().add(animal));
+        }else {
+            number = (int) (Math.random() * maxNumberOnCell);
+            IntStream.rangeClosed(0, number).forEachOrdered(value -> location.getPlants().add(new Grass()));
+        }
     }
 
 
---
+++ src/main/java/com/aleksei/animalisland/utils/enums/AnimalType.java
@@ -0,0 +1,19 @@
+package com.aleksei.animalisland.utils.enums;
+
+public enum AnimalType {
+    BEAR,
+    BOAR,
+    BUFFALO,
+    CATERPILLAR,
+    DEER,
+    DUCK,
+    EAGLE,
+    FOX,
+    GOAT,
+    HORSE,
+    MOUSE,
+    RABBIT,
+    SHEEP,
+    SNAKE,
+    WOLF
+}
